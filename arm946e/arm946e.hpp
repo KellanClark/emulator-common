f2fcdfc9d20f6b36e7b3adc26e76ee06d68f2153
Kellan Clark <kellanclark22@gmail.com>
2022-06-27 18:16:43 -0400

    I'm only doing this commit so Actions can show me the full error message

--- arm946e/arm946e.hpp
+++ arm946e/arm946e.hpp
@@ -1729,12 +1729,21 @@ public:
 	}
 
 	template <std::size_t... lutFillIndex>
-	constexpr static std::array<lutEntry, 8192> generateTable(std::index_sequence<lutFillIndex...>) {
-		return std::array{decode<lutFillIndex>()..., decode2<lutFillIndex>()...};
+	constexpr static std::array<lutEntry, 4096> generateTable(std::index_sequence<lutFillIndex...>) {
+		return std::array{decode<lutFillIndex>()...};
 	}
 
-	constexpr static const std::array<lutEntry, 8192> LUT = {
-		generateTable(std::make_index_sequence<8192>())
+	template <std::size_t... lutFillIndex>
+	constexpr static std::array<lutEntry, 4096> generateTable2(std::index_sequence<lutFillIndex...>) {
+		return std::array{decode2<lutFillIndex>()...};
+	}
+
+	constexpr static const std::array<lutEntry, 4096> LUT = {
+		generateTable(std::make_index_sequence<4096>())
+	};
+
+	constexpr static const std::array<lutEntry, 4096> LUT2 = {
+		generateTable2(std::make_index_sequence<4096>())
 	};
 
 	template <std::size_t lutFillIndex>
